7ef15f7f34fb6760d574d8ad347634402be8ce8d
Washington Charles de O Moraes <toncharles@live.com>
2019-04-27 16:26:31 -0300

    navegacao ajustada - IntroPage/TabsPage

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
 
 /**
  * Generated class for the IntroPage page.
@@ -15,11 +16,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class IntroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IntroPage');
+  }
+
+  goToTabsPage(){
+    this.navCtrl.push(TabsPage)
   }
 
 }
