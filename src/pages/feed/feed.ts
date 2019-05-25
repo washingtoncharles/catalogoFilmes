e0df70dd79e10e7b218304a38a1c6c34cd7ba5a7
Washington Charles de O Moraes <toncharles@live.com>
2019-05-25 19:52:11 -0300

    mensagem de carregamento de pagina

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { MovieProvider } from '../../providers/movie/movie';
 
 /**
@@ -13,33 +13,51 @@ import { MovieProvider } from '../../providers/movie/movie';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
-  providers:[
+  providers: [
     MovieProvider
   ]
 })
 export class FeedPage {
 
   public lista_filmes = new Array<any>();
+  public loader;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
-    private movieProvider: MovieProvider) {
+    private movieProvider: MovieProvider,
+    public loadingCtrl: LoadingController) {
   }
 
-  ionViewDidLoad() {
+  abreCarregando() {
+    this.loader = this.loadingCtrl.create({
+      content: "Carregando filmes...",
+      //duration: 3000 //tempo em milissegundos
+    });
+    this.loader.present();
+  }
+
+  fechaCarregando() {
+    this.loader.dismiss();
+  }
+
+
+  ionViewDidEnter() {
     //console.log('ionViewDidLoad FeedPage');
+    this.abreCarregando();
     this.movieProvider.getPopularMovies().subscribe(
-      data =>{
+      data => {
 
         const response = (data as any);
         const objeto_retorno = JSON.parse(response._body);
         this.lista_filmes = objeto_retorno.results;
-        
+
         console.log(objeto_retorno);
-    }, error =>{
-      console.log(error);
-    })
+        this.fechaCarregando();
+      }, error => {
+        console.log(error);
+        this.fechaCarregando();
+      })
   }
 
 }
